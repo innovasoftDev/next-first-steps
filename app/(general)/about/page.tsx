8178e614bde76767dbf9985951d2daa641d9fc17
InnovaSoft <innovasoft.dev.2024@gmail.com>
2024-12-01 14:46:37 -0600

    First commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,16 @@
+
+export const metadata = {
+ title: 'Acerca de',
+ description: 'Pagina de acerca de',
+ keywords: ['About Page','Fernando','información','...']
+};
+
+export default function AboutPage() {
+    return(
+        <>
+            <span className="text-7xl">
+                About Page
+            </span>
+        </>
+    )
+}
---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+ title: 'Contacto',
+ description: 'Página de Contacto',
+};
+
+export default function ContactPage() {
+    return(
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
